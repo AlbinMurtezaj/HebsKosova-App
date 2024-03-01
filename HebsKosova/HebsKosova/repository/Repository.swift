a5271bb2e5c0e1842a79aa1770116c465f8a7748
AlbinMurtezaj <122055604+AlbinMurtezaj@users.noreply.github.com>
2024-03-01 23:33:51 +0100

    Update Repository.swift
    Added another function to get data from table "Ofertat" form DB

--- HebsKosova/HebsKosova/repository/Repository.swift
+++ HebsKosova/HebsKosova/repository/Repository.swift
@@ -2,7 +2,7 @@
 //  Repository.swift
 //  HebsKosova
 //
-//  Created by Desara Qerimi on 27.2.24.
+//  Created by Albin Murtezaj on 27.2.24.
 //
 
 import Foundation
@@ -19,6 +19,40 @@ class Repository {
         }
         
         let query = "SELECT emri, cmimi FROM burgers_wraps"
+        var statement: OpaquePointer? = nil
+        
+        if sqlite3_prepare_v2(db, query, -1, &statement, nil) == SQLITE_OK {
+            while sqlite3_step(statement) == SQLITE_ROW {
+                if let emri = sqlite3_column_text(statement, 0),
+                   let cmimi = sqlite3_column_text(statement, 1){
+                    
+                    let emriString = String(cString: emri)
+                    let cmimiString = String(cString: cmimi)
+                    let fotoNo = photoCounter
+                    photoCounter += 1
+                    
+                    let foodModel = FoodModel(emri: emriString, cmimi: cmimiString, foto: fotoNo)
+                    foodModels.append(foodModel)
+                }
+            }
+        } else {
+            print("Error preparing query statement")
+        }
+        
+        sqlite3_finalize(statement)
+        
+        return foodModels
+    }
+    static func getDataOferta(db: OpaquePointer?) -> [FoodModel] {
+        var foodModels: [FoodModel] = []
+        var photoCounter: Int = 1
+        
+        guard let db = db else {
+            print("Database pointer is nil")
+            return []
+        }
+        
+        let query = "SELECT emri, cmimi FROM ofertat"
         var statement: OpaquePointer? = nil
         
         if sqlite3_prepare_v2(db, query, -1, &statement, nil) == SQLITE_OK {
